05dd6cf750d35bf4548b6ce57a4ff3ee95caf0ff
wolyoshu <98167164+wolyoshu@users.noreply.github.com>
2022-03-13 19:16:26 +0400

    Create isimler.js

---
+++ src/models/isimler.js
@@ -0,0 +1,8 @@
+const { Schema, model } = require('mongoose')
+
+const schema = Schema({
+  userID: { type: String, default: 0 },
+  guildID: { type: String, default: 0 },
+  names: { type: Array, default: [] }
+})
+module.exports = model("", schema)
